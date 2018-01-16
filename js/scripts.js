7b0624617a7b9d91f5e2b3c1a0d9643791033d6d
Woji <lubowicki.w@gmail.com>
2018-01-16 20:54:42 +0100

    Declared variables

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// scripts.js
+
+var a = 5,
+	h = 10,
+	triangleArea;
+
+triangleArea = a*h/2
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
